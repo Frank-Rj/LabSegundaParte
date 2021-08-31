6d36e76dda7798a7ef0f5cef710e585f75fa4df3
Frank-Rj <87914653+Frank-Rj@users.noreply.github.com>
2021-08-30 22:41:53 -0600

    Cambio Negocio

--- LabSegundaParte/LabSegundaParte/Negocio/ClsPersona.cs
+++ LabSegundaParte/LabSegundaParte/Negocio/ClsPersona.cs
@@ -8,5 +8,32 @@ namespace LabSegundaParte.Negocio
 {
     class ClsPersona
     {
+        public String CalculoIMC(Persona persona)
+        {
+            double resultado;
+            resultado = persona.Peso / Math.Pow(persona.Altura, 2);
+            Console.WriteLine("\nIndice de masa corporal: " + resultado);
+
+            if (resultado > 25)
+            {
+                return ("Tiene sobre peso.");
+            }
+            else
+            {
+                return ("Peso ideal.");
+            }
+        }
+
+        public String esMayorDeEdad(Persona persona)
+        {
+            if (persona.Edad > 17)
+            {
+                return ("Eres mayor de edad.");
+            }
+            else
+            {
+                return ("Eres menor de edad.");
+            }
+        }
     }
 }
